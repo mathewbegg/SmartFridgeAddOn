7fdcfe11b156d3246844f7dabc252fa1f60b3a1f
Bryan Philips <bryanphilips@ymail.com>
2021-02-20 01:17:23 -0500

    GUI Created

---
+++ Project/ImageCaptureSystem/ImageSender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace ImageCaptureSystem
+{
+    public class ImageSender
+    {
+
+        public static async Task<Mat> CaptureImage()
+        {
+
+            var cam = new ImageCapture();
+
+            try
+            {
+                cam.Initialize();
+                var image = cam.CaptureImage();
+                var i = await AWSUpload.UploadingFileAsync();
+                return image;
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                return null;
+
+            }
+
+        }
+        public static async Task<int> UploadImage()
+        {
+            var i = await AWSUpload.UploadingFileAsync();
+           return i;
+
+        }
+
+    }
+
+}
